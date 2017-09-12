ba8d475788b6899827416ff96c3e5c22ec3cd2e9
Dylan Grinberg <xkik-dev@users.noreply.github.com>
2017-09-11 19:18:44 -0700

    Added status text for alerting users about issues.

--- app/src/main/java/com/xkikdev/xkik/main_fragment.java
+++ app/src/main/java/com/xkikdev/xkik/main_fragment.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class main_fragment extends Fragment {
-
+    TextView statustxt;
 
     public main_fragment() {
         // Required empty public constructor
@@ -23,8 +24,26 @@ public class main_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_fragment, container, false);
+        View v = inflater.inflate(R.layout.fragment_main_fragment, container, false);// Inflate the layout for this fragment
+
+
+        statustxt= (TextView) v.findViewById(R.id.statusText);
+        new Thread(){
+            @Override
+            public void run() {
+                final String s = Util.urlToString("https://raw.githubusercontent.com/xkik-dev/xkik-status/master/status.txt");
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (s==null){
+                            statustxt.setText("Could not fetch status.");
+                        }
+                        statustxt.setText(s);
+                    }
+                });
+            }
+        }.start();
+        return v;
     }
 
 }
